adfa62131555d6fa2219f7b4bb4c90919625533d
xiatianming <xiatianming@yiguo.com>
2016-10-24 18:26:44 +0800

    [add] 添加建造者模式

---
+++ CreationalPattern/BuilderPattern.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationalPattern
+{
+    /*建造者模式
+    创建产品类，创建创建者父类
+    */
+    public class BuilderPattern
+    {
+        /// <summary>
+        /// 产品类
+        /// </summary>
+        public class Product
+        {
+            Queue queue = new Queue();
+
+            /// <summary>
+            /// 添加队列
+            /// </summary>
+            /// <param name="str"></param>
+            public void Add(string queue)
+            {
+                this.queue.Enqueue(queue);
+            }
+
+            public void Show()
+            {
+                foreach (var item in this.queue)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+            
+        }
+
+        /// <summary>
+        /// 建造者父类
+        /// </summary>
+        public class Builder
+        {
+            /// <summary>
+            /// 创建头
+            /// </summary>
+            public virtual void BuildHead() { }
+            /// <summary>
+            /// 创建身体
+            /// </summary>
+            public virtual void BuildBody() { }
+
+            /// <summary>
+            /// 创建手
+            /// </summary>
+            public virtual void BuildHands() { }
+
+            /// <summary>
+            /// 创建脚
+            /// </summary>
+            public virtual void BuildFeet() { }
+
+            /// <summary>
+            /// 获取产品
+            /// </summary>
+            /// <returns></returns>
+            public virtual Product GetResult() { return null; }
+        }
+
+
+        /// <summary>
+        /// 胖子建造者
+        /// </summary>
+        public class FatPersonBuilder : Builder
+        {
+            private Product product;
+            public FatPersonBuilder()
+            {
+                product = new Product();
+            }
+            public override void BuildHead()
+            {
+                product.Add("胖子增加头部\r\n");
+            }
+
+            public override void BuildBody()
+            {
+                product.Add("胖子增加身体\r\n");
+            }
+
+            public override void BuildHands()
+            {
+                product.Add("胖子增加手\r\n");
+            }
+
+            public override void BuildFeet()
+            {
+                product.Add("胖子增加脚\r\n");
+            }
+
+            public override Product GetResult()
+            {
+                return product;
+            }
+        }
+
+
+        /// <summary>
+        /// 瘦子建造者
+        /// </summary>
+        public class ThinPersonBuilder : Builder
+        {
+            private Product product=new Product();
+            public override void BuildHead()
+            {
+                product.Add("瘦子增加头部\r\n");
+            }
+
+            public override void BuildBody()
+            {
+                product.Add("瘦子增加身体\r\n");
+            }
+
+            public override void BuildHands()
+            {
+                product.Add("瘦子增加手\r\n");
+            }
+
+            public override void BuildFeet()
+            {
+                product.Add("瘦子增加脚\r\n");
+            }
+
+            public override Product GetResult()
+            {
+                return product;
+            }
+        }
+
+        /// <summary>
+        /// 导演类1
+        /// </summary>
+        public class Director1
+        {
+            public Director1(Builder builder)
+            {
+                builder.BuildHead();
+                builder.BuildBody();
+                builder.BuildHands();
+                builder.BuildFeet();
+            }
+        }
+
+        /// <summary>
+        /// 导演类2
+        /// </summary>
+        public class Director2
+        {
+            public Director2(Builder builder)
+            {
+                builder.BuildHead();
+                builder.BuildBody();
+            }
+        }
+
+
+
+
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -11,21 +11,38 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            ///简单工厂模式
             //SimpleFactoryPattern.Sender mail = SimpleFactoryPattern.SenderFactory.ProduceMail();
             //SimpleFactoryPattern.Sender sms=SimpleFactoryPattern.SenderFactory.ProduceSMS();
             //mail.Send();
             //sms.Send();
 
-            AbstractFactoryPattern.ProductA springA = new AbstractFactoryPattern.springFactoryC().createProductA();
-            ((AbstractFactoryPattern.SpringA)springA).setName("春天1");
-            ((AbstractFactoryPattern.SpringA)springA).getName();
+            ///抽象工厂模式
+            //AbstractFactoryPattern.ProductA springA = new AbstractFactoryPattern.springFactoryC().createProductA();
+            //((AbstractFactoryPattern.SpringA)springA).setName("春天1");
+            //((AbstractFactoryPattern.SpringA)springA).getName();
+            //AbstractFactoryPattern.ProductB springB = new AbstractFactoryPattern.springFactoryC().createProductB();
+            //((AbstractFactoryPattern.SpringB)springB).setName("春天2");
+            //((AbstractFactoryPattern.SpringB)springB).getName();
 
-            AbstractFactoryPattern.ProductB springB = new AbstractFactoryPattern.springFactoryC().createProductB();
-            ((AbstractFactoryPattern.SpringB)springB).setName("春天2");
-            ((AbstractFactoryPattern.SpringB)springB).getName();
+            ///建造者模式
+            /// 创建建造者类
+            BuilderPattern.Builder b1 = new BuilderPattern.FatPersonBuilder();
+            BuilderPattern.Builder b2 = new BuilderPattern.ThinPersonBuilder();
+            ///指定建造方式
+            BuilderPattern.Director1 director = new BuilderPattern.Director1(b1);
 
+            BuilderPattern.Director2 director2 = new BuilderPattern.Director2(b2);
 
+            ///通过建造者获取产品
+            BuilderPattern.Product fat= b1.GetResult();
+            ///产品展示
+            fat.Show();
 
+            ///通过建造者获取产品
+            BuilderPattern.Product thin = b2.GetResult();
+            //产品展示
+            thin.Show();
 
             Console.ReadLine();
 
