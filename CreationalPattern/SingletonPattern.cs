d7b7911f901de949d8fc749b62bb9e5db6b03863
tim-xia <summerming654027759@126.com>
2016-10-31 14:13:07 +0800

    [add] 新增单例模式 Singleton Pattern

---
+++ CreationalPattern/SingletonPattern.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationalPattern
+{
+    /* 单例模式
+    确保一个对象只会有一个实例化的对象存在,同时在多线程中也要确保这点
+    此处为懒汉模式。
+    饿汉模式是，对静态变量加 static readonly 关键字赋值。这样确保不存在多线程的问题。
+    优点是获取对象速度快，但是类加载速度慢些。懒汉模式跟它相反
+    */
+    public class SingletonPattern
+    {
+        /// <summary>
+        /// 定义私有静态单例类变量
+        /// </summary>
+        private static SingletonPattern instance;
+
+        /// <summary>
+        /// 定义私有锁，确保多线程下的单例模式
+        /// </summary>
+        private static object _lock = new object();
+
+        /// <summary>
+        /// 私有构造方法
+        /// </summary>
+        private SingletonPattern() { }
+
+        /// <summary>
+        /// 获取实例对象
+        /// </summary>
+        /// <returns></returns>
+        public static SingletonPattern GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (_lock)
+                {//实例为空时加锁
+                    if (instance == null)
+                    {//为实例初始化
+                        instance = new SingletonPattern();
+                        Console.WriteLine("执行了实例化");
+                    }
+                }
+            }
+            return instance;
+        }
+
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Test
@@ -25,27 +26,52 @@ namespace Test
             //((AbstractFactoryPattern.SpringB)springB).setName("春天2");
             //((AbstractFactoryPattern.SpringB)springB).getName();
 
-            ///建造者模式
-            /// 创建建造者类
-            BuilderPattern.Builder b1 = new BuilderPattern.FatPersonBuilder();
-            BuilderPattern.Builder b2 = new BuilderPattern.ThinPersonBuilder();
-            ///指定建造方式
-            BuilderPattern.Director1 director = new BuilderPattern.Director1(b1);
+            /////建造者模式
+            ///// 创建建造者类
+            //BuilderPattern.Builder b1 = new BuilderPattern.FatPersonBuilder();
+            //BuilderPattern.Builder b2 = new BuilderPattern.ThinPersonBuilder();
+            /////指定建造方式
+            //BuilderPattern.Director1 director = new BuilderPattern.Director1(b1);
 
-            BuilderPattern.Director2 director2 = new BuilderPattern.Director2(b2);
+            //BuilderPattern.Director2 director2 = new BuilderPattern.Director2(b2);
 
-            ///通过建造者获取产品
-            BuilderPattern.Product fat= b1.GetResult();
-            ///产品展示
-            fat.Show();
+            /////通过建造者获取产品
+            //BuilderPattern.Product fat= b1.GetResult();
+            /////产品展示
+            //fat.Show();
 
-            ///通过建造者获取产品
-            BuilderPattern.Product thin = b2.GetResult();
-            //产品展示
-            thin.Show();
+            /////通过建造者获取产品
+            //BuilderPattern.Product thin = b2.GetResult();
+            ////产品展示
+            //thin.Show();
 
+            Thread thread1 = new Thread(new ParameterizedThreadStart(ThreadDemo1));
+            Thread thread2 = new Thread(new ParameterizedThreadStart(ThreadDemo2));
+            thread1.IsBackground = true;
+            thread2.IsBackground = true;
+            thread1.Start("线程1");
+            thread2.Start("线程2");
+            Console.WriteLine("主线程执行结束");
             Console.ReadLine();
 
+        }
+
+        /// <summary>
+        /// 线程1调用方法
+        /// </summary>
+        public static  void ThreadDemo1(object str)
+        {
+            Console.WriteLine("执行了:" + str.ToString());
+            SingletonPattern single1 = SingletonPattern.GetInstance();
+        }
+
+        /// <summary>
+        /// 线程2调用方法
+        /// </summary>
+        public static void ThreadDemo2(object str)
+        {
+            Console.WriteLine("执行了:" + str.ToString());
+            SingletonPattern single2 = SingletonPattern.GetInstance();
         }
     }
 }
